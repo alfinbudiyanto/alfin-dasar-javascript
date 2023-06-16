182c60cbdc2c89df5384f6a496b3e63a93bb858c
alfinbudiyanto <alfinbudiyantogit@gmail.com>
2023-06-17 02:42:42 +0700

    alfin-pengulangan & else if

---
+++ latihan_2.2/angkot4.js
@@ -0,0 +1,12 @@
+const jmlAngkot = 10;
+const angkotBeroperasi = 6;
+
+for (let noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++) {
+  if (noAngkot <= 6) {
+    console.log("Angkot No. " + noAngkot + " beroperasi dengan baik.");
+  } else if (noAngkot === 8) {
+    console.log("Angkot No. " + noAngkot + " sedang lembur.");
+  } else {
+    console.log("Angkot No. " + noAngkot + " sedang tidak beroperasi.");
+  }
+}
