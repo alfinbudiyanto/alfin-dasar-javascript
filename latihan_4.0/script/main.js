82501b2e00e1d12359dbbe7a36139b152bb4ca1f
alfinbudiyanto <alfinbudiyantogit@gmail.com>
2023-06-29 02:41:54 +0700

    4.1 menyelesaikan fungsi tambah

--- latihan_4.0/script/main.js
+++ latihan_4.0/script/main.js
@@ -1,13 +1,27 @@
-let penumpang = ["Alfin", undefined, "Sandhika"];
+const penumpang = ["Sandhika", undefined, "Alfin"];
 
 const tambahPenumpang = function (namaPenumpang, penumpang) {
-  if (penumpang) {
+  if (penumpang.length == 0) {
     penumpang.push(namaPenumpang);
 
     return penumpang;
   } else {
-    // telusuri seluruh kursi dari awal
+    for (let i = 0; i < penumpang.length; i++) {
+      if (penumpang[i] == undefined) {
+        penumpang[i] = namaPenumpang;
+
+        return penumpang;
+      } else if (penumpang[i] == namaPenumpang) {
+        console.log(`${namaPenumpang} sudah ada di dalam angkot.`);
+
+        return penumpang;
+      } else if (i == penumpang.length - 1) {
+        penumpang.push(namaPenumpang);
+
+        return penumpang;
+      }
+    }
   }
 };
 
-tambahPenumpang();
+// tambahPenumpang();
